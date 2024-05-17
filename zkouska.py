753f3218c9ca0fb792ff4e1014561c043071cf39
pavelmares99 <mares.pa@protonmail.com>
2024-05-17 04:36:48 +0200

    test kalkulacka

--- zkouska.py
+++ zkouska.py
@@ -1,1 +1,10 @@
-print("Toto je zkouška!")
+import pytest
+import requests
+import kalkulacka
+
+def test_scitani():
+    assert kalkulacka.add(x: 2, y: 3) == 5
+    assert kalkulacka.add(x: 0, y: 1.5) == 1.5
+
+def test_deleni():
+    assert kalkulacka.divide(x: 2, y:0) == "Error Division by zero!"
